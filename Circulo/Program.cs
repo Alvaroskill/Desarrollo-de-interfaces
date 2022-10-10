eee896d40b8a01447e0942f18f6554ed2e086582
Alvaroskill <allinfer22@gmail.com>
2022-10-10 14:35:27 +0200

    Practica carnet

--- Circulo/Program.cs
+++ Circulo/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,15 @@ namespace Circulo
     {
         static void Main(string[] args)
         {
+            //Practica1();
+            Practica2();
+        }
+
+        #region Practica1
+
+        static void Practica1()
+        {
+
             Console.WriteLine("Introduzca el radio: ");
             int radio = int.Parse(Console.ReadLine());
 
@@ -18,6 +28,53 @@ namespace Circulo
             double area = PI * Math.Pow(radio, 2);
 
             Console.WriteLine($"El área del circulo de radio {radio} cm es {area} cm cuadrados");
+
+        }
+
+        #endregion
+
+
+        #region Practica2
+
+        static void Practica2()
+        {
+
+
+            const int MAYOR_EDAD = 18;
+            const string AFIRMACION = "S";
+
+            Console.WriteLine("Introduce la edad: ");
+            int edad = int.Parse(Console.ReadLine());
+
+
+
+            if (MAYOR_EDAD > edad)
+            {
+
+                Console.WriteLine("Eres menor de edad, no puedes conducir");
+
+            }
+            else if (MAYOR_EDAD >= edad)
+            {
+
+                Console.WriteLine("¿Tienes carnet?");
+                string carnet = Console.ReadLine().ToUpper();
+                if (carnet.Equals(AFIRMACION))
+                {
+                    Console.WriteLine("Puedes conducir");
+                }
+                else
+                {
+                    Console.WriteLine("No puedes conducir");
+                }
+
+
+
+
+            }
+
+            #endregion
+
         }
     }
 }
