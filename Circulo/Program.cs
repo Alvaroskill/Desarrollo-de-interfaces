e368b073c148f7bcee04650c245fcf327d21156c
Alvaroskill <allinfer22@gmail.com>
2022-10-10 13:11:24 +0200

    Agregar archivos de proyecto.

---
+++ Circulo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduzca el radio: ");
+            int radio = int.Parse(Console.ReadLine());
+
+            const double PI = 3.14;
+
+            double area = PI * Math.Pow(radio, 2);
+
+            Console.WriteLine($"El área del circulo de radio {radio} cm es {area} cm cuadrados");
+        }
+    }
+}
